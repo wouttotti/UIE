aecf91dab96dde67b5ab1487642b797d9bd59524
wouttotti <wout_kamp@hotmail.com>
2016-04-24 18:16:04 +0200

    Im done

--- Kneak/Kneak/app/src/main/java/com/kneak/wout/kneak/MainActivity.java
+++ Kneak/Kneak/app/src/main/java/com/kneak/wout/kneak/MainActivity.java
@@ -16,11 +16,15 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
 import android.widget.TextView;
@@ -54,19 +58,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        final FrameLayout myFrame = (FrameLayout) findViewById(R.id.pizzadiscount);
+        myFrame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set up dialog
+                CreateNewFrame();
+            }
+        });
+
         final FloatingActionButton myFab = (FloatingActionButton) findViewById(R.id.fab);
         myFab.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                //Creating the instance of PopupMenu
-                Dialog dialog = new Dialog(MainActivity.this);
+                //set up dialog
+                final Dialog dialog = new Dialog(MainActivity.this);
                 dialog.setContentView(R.layout.dialog_filter);
                 dialog.setTitle("This is my custom dialog box");
                 dialog.setCancelable(true);
+                Window window = dialog.getWindow();
+                WindowManager.LayoutParams wlp = window.getAttributes();
+
+                wlp.gravity = Gravity.BOTTOM;
+                wlp.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+                window.setAttributes(wlp);
                 //there are a lot of settings, for dialog, check them all out!
 
+                //set up text
+
                 //set up image view
-                ImageView img = (ImageView) dialog.findViewById(R.id.ImageView01);
+
+                //set up button
                 Button button = (Button) dialog.findViewById(R.id.Button01);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
                 //now that the dialog is set up, it's time to show it
                 dialog.show();
             }
@@ -74,7 +103,104 @@ public class MainActivity extends AppCompatActivity {
         //displayView(0);
 
     }
+    public void ResetFrame()
+    {
+        setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.show();
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_watchtv);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                selectDrawerItem(menuItem);
+                //Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+        final FrameLayout myFrame = (FrameLayout) findViewById(R.id.pizzadiscount);
+        myFrame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set up dialog
+                CreateNewFrame();
+            }
+        });
+
+        final FloatingActionButton myFab = (FloatingActionButton) findViewById(R.id.fab);
+        myFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //set up dialog
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.dialog_filter);
+                dialog.setTitle("This is my custom dialog box");
+                dialog.setCancelable(true);
+                Window window = dialog.getWindow();
+                WindowManager.LayoutParams wlp = window.getAttributes();
+
+                wlp.gravity = Gravity.BOTTOM;
+                wlp.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+                window.setAttributes(wlp);
+                //there are a lot of settings, for dialog, check them all out!
+
+                //set up text
+
+                //set up image view
+
+                //set up button
+                Button button = (Button) dialog.findViewById(R.id.Button01);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                //now that the dialog is set up, it's time to show it
+                dialog.show();
+            }
+        });
+    }
+
+    public void CreateNewFrame()
+    {
+        setContentView(R.layout.activity_discount_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.show();
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_watchtv);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                selectDrawerItem(menuItem);
+                //Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+        Button button = (Button) findViewById(R.id.buttonback);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ResetFrame();
+
+            }
+        });
+    }
     public void displayView(int position) {
 
         Fragment fragment = null;
